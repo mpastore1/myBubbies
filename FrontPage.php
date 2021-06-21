79909a4ddb1081cc219253253a83535e6c1646dc
John Doe <marco.pastore63@gmail.com>
2021-06-21 02:39:54 -0400

    8th commit

--- FrontPage.php
+++ FrontPage.php
@@ -21,16 +21,23 @@
 <a href="Photos.php">  Photos </a>
 <a href="Fav.php">  Your Favorites </a>
 </div>
+<div>
+<h1></h1>
 
+</div>
+<div>
+<h1></h1>
+
+</div>
 <div class="container">
 <img src='pics/hearts.gif' style= 'width:100%; height:100%;' />
-<h2 class="centered" style="color:black; font-size:1.8vw" >I met my bubbies on March 12, 2020, and we went to the aroma espresso bar. <br>
-My bubbies and I had tuna sandwiches and large coffees. That day we loved spending so much time together<b>
+<p class="centered" style="color:black; font-size:2.7vw" ><b>I met my bubbies on March 12, 2020, and we went to the aroma espresso bar. <br>
+My bubbies and I had tuna sandwiches and large coffees. That day we loved spending so much time together
 we decided to go watch a movie(we watched the invisible man). On March 17th Ontario went into its first<br>
 lockdown and we didn't see each other after for a while. Then on May 15th, I texted my bubbies asking to go for a walk<br>
 and we went on to venturing into forest trails which we still enjoy, even today. From then on we took up picnicking and tried as many restaurants as we could<br>
 then on June 26th, we traveled to Oshawa at Gordon Richards Park on the waterfront where we kissed for the first time.&#10084; <br>
-Then on this day, July 1st we made our love for each other official and I couldn't be happier. &#10084;&#10084;&#10084;   &#10084;&#10084;&#10084;  </h2>
+Then on this day, July 1st we made our love for each other official and I couldn't be happier.</b> &#10084;&#10084;&#10084;   &#10084;&#10084;&#10084;  </p>
 
 </div>
 </body>
