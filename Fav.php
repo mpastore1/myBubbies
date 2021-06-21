4fabcdbb5f96fc334e42b46bcdb46105c579a75e
John Doe <marco.pastore63@gmail.com>
2021-06-21 15:07:41 -0400

    10th commit

--- Fav.php
+++ Fav.php
@@ -36,25 +36,25 @@
 <td><a href="https://www.google.com/search?q=sushi&rlz=1C1CHBF_enCA899CA899&sxsrf=ALeKk01IKKi0thWcc2zP_Nw1oW-3LgIgJg:1624301266588&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjZ1YLgsanxAhXymOAKHcteCk8Q_AUoAnoECAEQBA&biw=1366&bih=625" target="_blank" style="color:white;"> Sushi</a> </td>
 <td><a href="https://www.google.com/search?q=sunset%20point&rlz=1C1CHBF_enCA899CA899&oq=sunset+point&aqs=chrome.0.69i59.105813j0j9&sourceid=chrome&ie=UTF-8&tbs=lf:1,lf_ui:2&tbm=lcl&sxsrf=ALeKk02ePHLMi50CiWrZqNaXeLQ8uyHUGQ:1624301626930&rflfq=1&num=10&rldimm=4956634944273180092&lqi=CgxzdW5zZXQgcG9pbnRI8NKfn-uAgIAIWhYQABABGAAYASIMc3Vuc2V0IHBvaW50kgEMcHVibGljX2JlYWNomgEjQ2haRFNVaE5NRzluUzBWSlEwRm5TVU5CWjBzdFNscG5FQUWqARQQASoQIgxzdW5zZXQgcG9pbnQoAA&ved=2ahUKEwih_euLs6nxAhWQEVkFHTKrBDoQvS4wAHoECAQQOg&rlst=f#rlfi=hd:;si:4956634944273180092,l,CgxzdW5zZXQgcG9pbnRI8NKfn-uAgIAIWhYQABABGAAYASIMc3Vuc2V0IHBvaW50kgEMcHVibGljX2JlYWNomgEjQ2haRFNVaE5NRzluUzBWSlEwRm5TVU5CWjBzdFNscG5FQUWqARQQASoQIgxzdW5zZXQgcG9pbnQoAA;mv:[[45.104217299999995,-79.3110835],[43.6006541,-80.2592643]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!1m4!1u16!2m2!16m1!1e1!1m4!1u16!2m2!16m1!1e2!2m1!1e2!2m1!1e16!2m1!1e3!2m4!1e17!4m2!17m1!1e2!3sIAE,lf:1,lf_ui:2 "target="_blank" style="color:white;"> Sunset Point </td>
 <td><a href="https://youtube.com/playlist?list=PL86he_xG9Ku2JQGBLSGHsR7e2ARbrpd9U"target="_blank" style="color:white;"> Doja Cat </a></td>
-<td> New Girl </td>
+<td style="color:white;"> New Girl </td>
 </tr>
 <tr>
 <td><a href="https://www.google.com/search?q=sweet+treats&tbm=isch&ved=2ahUKEwiBm4iNsqnxAhUCAc0KHZGGDfEQ2-cCegQIABAA&oq=sweet+treats&gs_lcp=CgNpbWcQAzIECAAQQzIECAAQQzIECAAQQzIECAAQQzIECAAQQzIECAAQQzICCAAyBAgAEEMyBAgAEEMyAggAUKgQWKgQYLoSaABwAHgAgAFfiAFfkgEBMZgBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=Md_QYMG-AoKCtAaRjbaIDw&bih=625&biw=1366&rlz=1C1CHBF_enCA899CA899" target="_blank"style="color:white;"> Sweet treats </a></td>
 <td><a href="https://www.google.com/search?q=trails&rlz=1C1CHBF_enCA899CA899&tbm=lcl&sxsrf=ALeKk00VjI8GUmC_tog_K5WKR9-HgW6aoA%3A1624301631356&ei=P-DQYN2hFdHQtQb74KWYDQ&oq=trails&gs_l=psy-ab.3..0i433k1j0l3j0i457k1j0i402k1j0j0i433k1j0l2.83741.85383.0.86289.7.7.0.0.0.0.102.459.3j2.6.0....0...1c.1.64.psy-ab..1.6.578.10..35i362i39k1j35i39k1j0i67k1j0i433i131k1j0i273k1j0i433i131i67k1j0i263i433i131i20k1j0i263i20k1j0i433i457k1j0i433i457i67k1.122.Hm5HCDjC8BY#rlfi=hd:;si:;mv:[[43.968847,-79.3358801],[43.7208323,-79.6352278]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!1m4!1u16!2m2!16m1!1e1!1m4!1u16!2m2!16m1!1e2!2m1!1e2!2m1!1e16!2m1!1e3!2m4!1e17!4m2!17m1!1e2!3sIAE,lf:1,lf_ui:1" target="_blank" style="color:white;"> Trailes </a></td>
 <td><a href="https://youtube.com/playlist?list=PLu9Vz-S8dcTFRigm63_S_bIQ3z_NvdOX9"target="_blank" style="color:white;"> Adele </a></td>
-<td >House Hunters</td>
+<td style="color:white;">House Hunters</td>
 </tr>
 <tr>
 <td><a href="https://www.google.com/search?q=BOHO+food&tbm=isch&ved=2ahUKEwjtw5S1sqnxAhX1A50JHc_ID78Q2-cCegQIABAA&oq=BOHO+food&gs_lcp=CgNpbWcQAzICCAAyAggAMgIIADICCAAyAggAMgIIADICCAAyAggAMgYIABAFEB4yBggAEAUQHjoHCCMQ6gIQJzoECCMQJzoECAAQQzoHCAAQsQMQQzoFCAAQsQNQ15gBWPDCAWC-xAFoA3AAeACAAYUBiAHcBpIBBDEwLjGYAQCgAQGqAQtnd3Mtd2l6LWltZ7ABCsABAQ&sclient=img&ei=hd_QYK3tB_WH9PwPz5G_-As&bih=625&biw=1366&rlz=1C1CHBF_enCA899CA899"target="_blank" style="color:white;"> Boho food </a></td>
 <td> <a href="https://www.google.com/search?q=camping&rlz=1C1CHBF_enCA899CA899&tbm=lcl&sxsrf=ALeKk0165wzNm0HvGidlhDI9NyFT3CmEyA%3A1624301718724&ei=luDQYLvGK9CrtQbjoYvoBw&oq=camping&gs_l=psy-ab.3..35i39k1l2j0i273k1j0i433i131i457i67k1j0i402k1l2j0i67k1l2j0i433i131k1j0i433k1.65292.66717.0.66835.8.8.0.0.0.0.115.689.4j3.8.0....0...1c.1.64.psy-ab..0.8.866.10..0j35i362i39k1j0i457i67k1.179.OOKXY2aqXJE#rlfi=hd:;si:;mv:[[44.608329399999995,-78.7838448],[42.998803099999996,-80.078886]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!1m4!1u16!2m2!16m1!1e1!1m4!1u16!2m2!16m1!1e2!2m1!1e2!2m1!1e16!2m1!1e3!2m4!1e17!4m2!17m1!1e2!3sIAE,lf:1,lf_ui:1" target="_blank" style="color:white;"> Camping </td>
 <td> <a href="https://www.youtube.com/watch?v=kvazBqAlx58"target="_blank" style="color:white;">Paul Anka </td>
-<td>Keeping up with the Kardashians </td>
+<td style="color:white;">Keeping up with the Kardashians </td>
 </tr>
 <tr>
 <td> <a href="https://www.google.com/search?q=Smoothies&tbm=isch&ved=2ahUKEwir1tvBsqnxAhWPNc0KHayFD-wQ2-cCegQIABAA&oq=Smoothies&gs_lcp=CgNpbWcQAzIHCAAQsQMQQzICCAAyAggAMgIIADICCAAyAggAMgIIADICCAAyAggAMgIIADoHCCMQ6gIQJzoECCMQJzoFCAAQsQM6CAgAELEDEIMBOgQIABBDUPyXAliiqQJgl6oCaAFwAHgAgAGwAYgBvAaSAQM4LjGYAQCgAQGqAQtnd3Mtd2l6LWltZ7ABCsABAQ&sclient=img&ei=n9_QYOuKHI_rtAasi77gDg&bih=625&biw=1366&rlz=1C1CHBF_enCA899CA899" target="_blank" style="color:white;">Smoothies </a></td>
 <td> <a href="https://www.google.com/search?q=65+Larratt+Lane%2C+Richmond+Hill%2C+ON&rlz=1C1CHBF_enCA899CA899&tbm=lcl&sxsrf=ALeKk03cB4IemNp4FkD_fciFr2cRIcxBnA%3A1624301786642&ei=2uDQYMbJJs2ztQaiwZB4&oq=65+larra&gs_l=psy-ab.3.0.38j0i22i30k1j38.38516.40406.0.41551.9.9.0.0.0.0.105.710.7j1.9.0....0...1c.1.64.psy-ab..0.9.870.10..0j35i362i39k1j0i273k1j0i433k1j0i433i131k1j0i433i67k1j0i67k1j0i263i20k1j0i22i10i30k1.163.c9cJzEXFQ0A"target="_blank" style="color:white;"> &#10084; Marco's House &#10084; </a></td>
 <td> <a href="https://youtube.com/playlist?list=PLWMCdW_trWGAYv3CpXHQ1H4apg426-MNJ"target="_blank" style="color:white;">Cardi B </td>
-<td> Friends</td>
+<td style="color:white;"> Friends</td>
 
 </tr>
 
