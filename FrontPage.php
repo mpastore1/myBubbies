2fc6612347697d558702f488f6711c937b34dd5c
John Doe <marco.pastore63@gmail.com>
2021-06-21 15:03:51 -0400

    9th commit

--- FrontPage.php
+++ FrontPage.php
@@ -9,10 +9,9 @@
     <th>&#10084; Happy 1 year Bubbies &#10084;</th>
     <th><img src='/pics/tenor.gif' style="float:left; width:100%;height:20%;" /></th>    
 </tr>
-
-
 </table>
 </div>
+
 <body>
 
 
@@ -29,14 +28,14 @@
 <h1></h1>
 
 </div>
-<div class="container">
+<div class="container" >
 <img src='pics/hearts.gif' style= 'width:100%; height:100%;' />
-<p class="centered" style="color:black; font-size:2.7vw" ><b>I met my bubbies on March 12, 2020, and we went to the aroma espresso bar. <br>
+<p class="centered" style="color:black; font-size:2.7vw;" ><b>I met my bubbies on March 12, 2020, and we went to the aroma espresso bar. 
 My bubbies and I had tuna sandwiches and large coffees. That day we loved spending so much time together
-we decided to go watch a movie(we watched the invisible man). On March 17th Ontario went into its first<br>
-lockdown and we didn't see each other after for a while. Then on May 15th, I texted my bubbies asking to go for a walk<br>
-and we went on to venturing into forest trails which we still enjoy, even today. From then on we took up picnicking and tried as many restaurants as we could<br>
-then on June 26th, we traveled to Oshawa at Gordon Richards Park on the waterfront where we kissed for the first time.&#10084; <br>
+we decided to go watch a movie(we watched the invisible man). On March 17th Ontario went into its first
+lockdown and we didn't see each other after for a while. Then on May 15th, I texted my bubbies asking to go for a walk
+and we went on to venturing into forest trails which we still enjoy, even today. From then on we took up picnicking and tried as many restaurants as we could
+then on June 26th, we traveled to Oshawa at Gordon Richards Park on the waterfront where we kissed for the first time.&#10084;
 Then on this day, July 1st we made our love for each other official and I couldn't be happier.</b> &#10084;&#10084;&#10084;   &#10084;&#10084;&#10084;  </p>
 
 </div>
